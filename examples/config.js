212b0c97975c58635570a03213b2697fb4f25e2b
Chris Dituri <csdituri@gmail.com>
2015-03-31 19:06:08 -0500

    examples/config.js: add missing commas to end of Object fields
    Pointed out by a friendly user via forum post. Essentially the commas
    delimiting oauthConfig Object's fields were accidentilly scrubbed
    before first shipment. Fixed.

--- examples/config.js
+++ examples/config.js
@@ -1,9 +1,9 @@
 // Setup key/secret for authentication and API endpoint URL
 var oauthConfig = {
   apiUrl: "https://api.tradeking.com/v1",
-  consumerKey: "OBTAIN"
-  consumerSecret: "THESE"
-  accessToken: "FROM"
+  consumerKey: "OBTAIN",
+  consumerSecret: "THESE",
+  accessToken: "FROM",
   accessSecret: "TRADEKING"
 }
 
